b9eb77679c98eec3d8a70f6641ffc0ffe79e341a
Dmitrii Lavrov <lavrovdmitrii@gmail.com>
2023-04-19 23:45:34 +0200

    4e

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,11 @@ import React from 'react';
 import PageTitle from './pageTitle/PageTitle';
 import EventBoard from './EventsBoard/EventsBoard';
 import upcomingEvents from '../upcoming-events.json'
-import { FaBeer } from 'react-icons/fa';
+
 
 
 const App = () => {
 	return <div>
-				<FaBeer />
 				<PageTitle text="24th Core Worlds Coalition Confernce"/>
 				<EventBoard events={upcomingEvents}/>
 		   </div>;
--- src/components/event/Event.jsx
+++ src/components/event/Event.jsx
@@ -1,27 +1,37 @@
+import { FaCalendarAlt } from "react-icons/fa";
+import { FaClock } from "react-icons/fa";
+import { FaMapMarkerAlt, FaUserAlt, } from "react-icons/fa";
+import { formatEventStart } from "utils/formatEventStart";
+import { formatEventDuration } from "utils/formatEventsDuration";
+
 import PropTypes from 'prop-types';
 import css from './Event.module.css';
 
+
+
 export const Event = ({ name, location, speaker, type, start, end}) => {
+    const formattedStart = formatEventStart(start);
+    const duration = formatEventDuration(start, end)
     return (
         <div className={css.event}>
         <h2 className={css.title}>{name}</h2>
         <p className={css.info}>
-            <i className={css.icon}></i>
+            <FaMapMarkerAlt />
             {location}
         </p>
         <p className={css.info}>
-            <i className={css.icon}></i>
+            <FaUserAlt className={css.icon} size={26} />
             {speaker}
         </p>
         <p className={css.info}>
-            <i className={css.icon}></i>
-            {start}
+            <FaCalendarAlt />
+            {formattedStart}
         </p>
         <p className={css.info}>
-            <i className={css.icon}></i>
-            Duration
+            <FaClock />
+            {duration}
         </p>
-        {/* <span class="chip free|paid|vip">Evemt Type</span> */}
+        <span className={css.chip}>{type}</span>
         </div>
     );
 };
